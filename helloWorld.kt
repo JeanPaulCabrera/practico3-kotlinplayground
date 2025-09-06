067569c40bea91573fbb932999bfcf5190e784cb
JeanPaulCabreraFlores <126268240+lomandopuntocom@users.noreply.github.com>
2025-09-05 20:47:37 -0400

    Saludo

---
+++ helloWorld.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val nombre: String = "Jean Paul Cabrera Flores"
+    val nombreMateria: String = "Aplicaciones Móviles"
+
+    println("$nombre $nombreMateria")
+    println("Peso: $peso kg")
+}
