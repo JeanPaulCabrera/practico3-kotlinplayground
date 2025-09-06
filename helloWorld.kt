e7c1fe18acd42075989d51fc9fb9c2a7a3775d15
JeanPaulCabreraFlores <126268240+lomandopuntocom@users.noreply.github.com>
2025-09-05 20:59:55 -0400

    Saludov2

--- helloWorld.kt
+++ helloWorld.kt
@@ -1,7 +1,20 @@
+enum class Sexo {
+    HOMBRE, MUJER
+}
+
 fun main() {
     val nombre: String = "Jean Paul Cabrera Flores"
     val nombreMateria: String = "Aplicaciones Móviles"
+    val peso: Int = 70
+    val altura: Int = 171
+    val edad: Int = 22
+    val sexo = Sexo.HOMBRE
+    val esPar: Boolean = if (edad % 2 == 0) true else false
 
     println("$nombre $nombreMateria")
     println("Peso: $peso kg")
+    println("Altura: $altura cm")
+    println("Edad: $edad")
+    println("Sexo: $sexo")
+    println("Edad par: ${if (esPar) "Sí" else "No"}")
 }
